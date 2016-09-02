6803577767b5a7b51d27c3f6ec021790b00a530b
Vladislav Petrov <electronicchest@gmail.com>
2016-09-02 13:05:59 +0300

    Replace sleep with calculating

--- app/controllers/gds_controller.rb
+++ app/controllers/gds_controller.rb
@@ -8,11 +8,13 @@ class GdsController < ApplicationController
     # tickets_compression - 0.0007
     # full_search - 3.5408
 
-    sleep 2.915 # search_request
-    sleep 0.000 # split_ticketing
-    sleep 0.098 # bundles_merging
-    sleep 0.432 # rates_application
-    sleep 0.005 # nested_tickets_building
-    sleep 0.001 # tickets_compression
+    # sleep 2.915 # search_request
+    # sleep 0.000 # split_ticketing
+    # sleep 0.098 # bundles_merging
+    # sleep 0.432 # rates_application
+    # sleep 0.005 # nested_tickets_building
+    # sleep 0.001 # tickets_compression
+
+    10_000_000.times { 569123 ** 5 }
   end
 end
